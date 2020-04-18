45036a26d69dff410038f61257f77f40a7d1de7f
Martin Hellspong <martin.hellspong@factor10.com>
2020-04-18 14:50:21 +0200

    Don't convert timing to absolute prematurely

--- komp-cli/src/play.rs
+++ komp-cli/src/play.rs
@@ -48,37 +48,31 @@ fn setup_events(channel: u8) -> Vec<Event> {
     events
 }
 
-fn distribute<'a>(
-    events: &'a Vec<Event>,
-    ppq: u32,
-    len: u8,
-    ms_per_quarter: u32,
-) -> Vec<TimedEvent<'a>> {
+fn distribute<'a>(events: &'a Vec<Event>, ppq: u32, len: u8) -> Vec<TimedEvent<'a>> {
     let mut timed_events = vec![];
-    let us_per_tick = 1000 * ms_per_quarter / ppq;
     for quarter in 0..4 {
         timed_events.push(TimedEvent {
-            timing: quarter * ppq * us_per_tick,
+            timing: quarter * ppq,
             event: &events[0],
         });
         timed_events.push(TimedEvent {
-            timing: quarter * ppq * us_per_tick,
+            timing: quarter * ppq,
             event: &events[1],
         });
         timed_events.push(TimedEvent {
-            timing: quarter * ppq * us_per_tick,
+            timing: quarter * ppq,
             event: &events[2],
         });
         timed_events.push(TimedEvent {
-            timing: (quarter * ppq + len as u32) * us_per_tick,
+            timing: quarter * ppq + len as u32,
             event: &events[3],
         });
         timed_events.push(TimedEvent {
-            timing: (quarter * ppq + len as u32) * us_per_tick,
+            timing: quarter * ppq + len as u32,
             event: &events[4],
         });
         timed_events.push(TimedEvent {
-            timing: (quarter * ppq + len as u32) * us_per_tick,
+            timing: quarter * ppq + len as u32,
             event: &events[5],
         });
     }
@@ -87,9 +81,13 @@ fn distribute<'a>(
 
 pub fn schedule_music(timestamp: u64, key: Key) -> coremidi::PacketBuffer {
     let events = setup_events(1);
-    let tes = distribute(&events, 96, 80, 500_000);
+    let ppq = 96;
+    let tes = distribute(&events, ppq, 80);
 
     let mut packet_buf = coremidi::PacketBuffer::with_capacity(512);
+    let ms_per_quarter = 500_000;
+
+    let us_per_tick = 1000 * ms_per_quarter / ppq;
 
     for te in tes.iter() {
         let data = match te.event {
